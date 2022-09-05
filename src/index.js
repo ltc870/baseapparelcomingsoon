918787c6d077ca83817b59b301da27e620b72061
Lawrence C <lawrencecaudle870@gmail.com>
2022-09-05 11:38:09 -0400

    added javascript to index.js

---
+++ src/index.js
@@ -0,0 +1,28 @@
+"use strict";
+
+const form = document.querySelector("#form");
+const email = document.querySelector("#email");
+const errorIcon = document.querySelector("#error-icon");
+const errorMsg = document.querySelector("#error-msg");
+const regEx =
+    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+errorMsg.style.visibility = "hidden";
+errorIcon.style.visibility = "hidden";
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    if (email.value.match(regEx)) {
+        alert("Thank you for your submission!!");
+        form.submit();
+    } else {
+        errorMsg.style.visibility = "visible";
+        errorIcon.style.visibility = "visible";
+    }
+});
+
+email.addEventListener("input", () => {
+    errorMsg.style.visibility = "hidden";
+    errorIcon.style.visibility = "hidden";
+});
